73eb306d13fe92459f113031e81ecf2a077bb0ba
pato <patooodv@gmail.com>
2024-06-08 18:18:45 -0300

    fix en obligatorio 1

--- obligatorio_1/obligatorio.ts
+++ obligatorio_1/obligatorio.ts
@@ -1,10 +1,10 @@
 import * as readline from 'readline-sync';
 
-let primervuelta: number = readline.questionInt("Ingrese la primer vuelta");
-console.log("La primer vuelta fue ", primervuelta);
-let segundavuelta: number = readline.questionInt("Ingrese la segunda vuelta");
-console.log("La segunda vuelta fue ", segundavuelta);
-let tercervuelta: number = readline.questionInt("Ingrese la tercer vuelta");
-console.log("La tercer vuelta fue ", tercervuelta);
+let primervuelta: number = readline.questionInt("Ingrese la primer vuelta: ");
+console.log("La primer vuelta fue: ", primervuelta);
+let segundavuelta: number = readline.questionInt("Ingrese la segunda vuelta: ");
+console.log("La segunda vuelta fue: ", segundavuelta);
+let tercervuelta: number = readline.questionInt("Ingrese la tercer vuelta: ");
+console.log("La tercer vuelta fue: ", tercervuelta);
 
 console.log(primervuelta+segundavuelta+tercervuelta);
